00f6fc96f46f1fdcae8d461dddc68ffcbc253b50
estefania repetti <repetti.stefania@gmail.com>
2022-11-05 19:50:56 -0300

    cambios en la web 2

--- js/form.js
+++ js/form.js
@@ -1,90 +1,34 @@
-var btn_send = document.getElementById("sendBtn");
-btn_send.addEventListener("click", sendMessage);
-
-const name = document.getElementById("name").value;
-const email = document.getElementById("email").value;
-const message = document.getElementById("message").value;
-
-function isMail() {
-    /*var email_text = document.getElementById("email").value;
-    var regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3,4})+$/;
-    if (!regex.test(email_text)) {
-        return false;
-    } else {
-        return true;
-    }*/ return true ;
-}
-function isNombre() {
-    /*var name_text = document.getElementById("name").value;
-    var regex = [A - Za - z];
-    if (!regex.test(email_text)) {
-        return false;
-    } else{
-        return true;
-    }
-*/return true;
-}
+/*NEW FORM */
 
 
-function sendMessage(event) {
-    event.preventDefault();
-    if (isNombre()) {
-        if (isMail()) {
-            var nameForm = (document.getElementById("name").value).trim();
-            var emailForm = (document.getElementById("email").value).trim();
-            var messageForm = (document.getElementById("message").value).trim();
-            var connect, form, asunto, recaptcha_web_key, recaptcha_web_response;
-            
-            recaptcha_web_key = '6LdUv9AdAAAAABFY2Cq-42JbyofqFRaNUp_y87bC'; //copiar clave 
-            // start recaptcha
+const btn = document.getElementById('button');
 
-            grecaptcha.ready(function () {
-                grecaptcha.execute(recaptcha_web_key, { action: 'contact' }).then(function (g_token) {
-                    recaptcha_web_response = (g_token != '') ? g_token : 'disabled';
+document.getElementById('form')
+    .addEventListener('submit', function (event) {
+        event.preventDefault();
 
-                    form = 'name=' + nameForm + '&email=' + emailForm + '&message=' + messageForm + '&recaptcha_web_response=' + recaptcha_web_response;
+        btn.value = 'Sending...';
 
-                    connect = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
+        const serviceID = 'default_service';
+        const templateID = 'template_qjv1yy9';
 
-                    connect.onreadystatechange = function () {
-                        if (connect.readyState == 4 && connect.status == 200) {
-                            //console.log("response: " + connect.responseText);
-                            var response = JSON.parse(connect.responseText);
-                            console.log(response);
+        emailjs.sendForm(serviceID, templateID, this)
+            .then(() => {
+                btn.value = 'Send';
                 
-                            if (response.success) {
-                                document.getElementById("name").value = "";
-                                document.getElementById("email").value = "";
-                                document.getElementById("message").value = "";
-                                window.location.replace('/Sein');
-                                alert("Email send succefully")
-                               // form_success_div.style.display = "block"; 
-                            } else {
-                                alert("Something went wrong please try later")
-                                //form_fail_div.style.display = "block";
-                            }
-                        } else if (connect.readyState != 4) {
-                            //Processing the request
-                            //botonEnviar.innerHTML = '<p>Enviando...</p><i class="fas fa-circle-notch fa-spin"></i>'
-                           // botonEnviar.disabled = true
-                        }
-                    };
-                    connect.open("POST", "./php/form.php", true);
-                    connect.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-                    connect.send(form);
-                    // end submit form
-                });
+            
+            }, (err) => {
+                btn.value = 'Send';
+    
+            }           
+            );
+
+    });
+
+    const button = document.getElementById("button");
+    const inputId = document.getElementById("input-Id");
+    
+            button.addEventListener("click", function () {
+              inputId.value = "";
+
             });
-            // end reCaptcha
-
-        } else {
-            alert("Mail incorrecto")
-        }
-    } else {
-        alert("Ingrese su nombre")
-    }
-
-
-
-
-}
--- js/sein.js
+++ js/sein.js
@@ -1,10 +1,52 @@
 
 const navbarHeight = $('#navbar').outerHeight();
-
 $('.navbar ul li a').click(function () {
     $('html, body').animate({
         scrollTop: $($.attr(this, 'href')).offset().top - navbarHeight
     }, 100);
     return false;
 });
+/*
+
+let isActive = false;
+
+$('.js-menu').on('click', function() {
+	if (isActive) {
+		$(this).removeClass('active');
+		$('body').removeClass('menu-open');
+	} else {
+		$(this).addClass('active');
+		$('body').addClass('menu-open');
+	}
+
+	isActive = !isActive;
+});
+*/
+
+/*NUEVO*/ 
+var menu = document.querySelector('.hamburger');
+
+// method
+function toggleMenu (event) {
+    this.classList.toggle('is-active');
+    document.querySelector( ".menuppal" ).classList.toggle("is_active");
+    event.preventDefault();
+  }
+  
+  // event
+  menu.addEventListener('click', toggleMenu, false);
+
+/* TOP */
+let topDos = $(".topDos");
+let topDosIndex = -1;
+
+function showNextTop(){
+    ++topDosIndex;
+    topDos.eq(topDosIndex % topDos.length)
+    .fadeIn(400)
+    .delay(600)
+    .fadeOut(800, showNextTop);
+}
+
+showNextTop();
 
